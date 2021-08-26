083edbdbbc3cfe9d904449629e2060b473c4234e
phxmb <61689592+phxmb@users.noreply.github.com>
2021-08-26 16:15:39 +0100

    Update Horizon_FSLogix_Teams_OneDrive.ps1

--- Horizon_FSLogix_Teams_OneDrive.ps1
+++ Horizon_FSLogix_Teams_OneDrive.ps1
@@ -7,7 +7,7 @@ Start-Process -FilePath 'C:\gold\fslogix\x64\Release\FSLogixAppsSetup.exe' -Argu
 Start-BitsTransfer -Source 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true' -Destination 'c:\gold\Teams_windows_x64.msi' -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -Force | Out-Null
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -Name 'IsWVDEnvironment' -Value '1' -PropertyType DWORD -Force | Out-Null
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\gold\Teams_windows_x64.msi /quiet /l*v C:\gold\teamsinstall.log ALLUSER=1 ALLUSERS=1 OPTIONS="noAutoStart=true'
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\gold\Teams_windows_x64.msi /l*v C:\gold\teamsinstall.log ALLUSER=1 ALLUSERS=1'
 
 #OneDrive pre Machine Installation#
 Start-BitsTransfer -Source 'https://go.microsoft.com/fwlink/?linkid=844652' -Destination 'c:\gold\OneDriveSetup.exe' -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits
